0e9a9907d191430b1ad3dcf2bb1dc9a19a05d65b
Park, Jonghyeon <belline0124@gmail.com>
2024-05-30 19:30:03 +0900

    Remove cause of plantuml runtime error

--- render.sh
+++ render.sh
@@ -1,4 +1,4 @@
 rm -rf rendered
-find . -name "*.puml" -exec sh -c 'plantuml -DPLANTUML_LIMIT_SIZE=16384 $0 -o "$(pwd)"/rendered/"${0%/*.puml}"' {} \; 
+find . -name "*.puml" -exec sh -c 'plantuml -DPLANTUML_LIMIT_SIZE=20000 $0 -o "$(pwd)"/rendered/"${0%/*.puml}"' {} \; 
 mv rendered/src/* rendered
 rm -rf rendered/src
